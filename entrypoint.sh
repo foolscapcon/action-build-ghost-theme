88f1bb37cd788aa42e8fb4a1c1b14568cd621d40
Douglas Beal <public.git@dougbeal.com>
2019-09-22 19:02:05 -0700

    this seems to be the format

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,9 @@ if [ -e $target ]; then
     rm $target
 fi
 zip $target -r * -x \*${working}\* -x \*.git\* -x \*~
-echo ::set-output name=file::"${target}"
-echo ::set-output name=name::"${name}"
-echo ::set-output name=tag::"${tag}"
+# https://github.com/actions/toolkit/blob/2c6d31be8f9c8201d3cc9384133b933543b69075/docs/commands.md
+echo ::set-output name=release_file::"${target}"
+echo ::set-output name=release_name::"${name}"
+echo ::set-output name=release_tag::"${tag}"
 
+#CMD_STRING + this.command + ' ' + '$key=$val' + :: + $message
